e91df274101a7717fc7bef5375f30e1bcf4e94fa
YunqianAo <79161526+YunqianAo@users.noreply.github.com>
2023-01-06 03:51:20 +0100

    ModulePhysics.h
    Copy the class given by the teacher and follow the teacher´s way to create the class"cube"

--- Physics II - 2D Template/ModulePhysics.h
+++ Physics II - 2D Template/ModulePhysics.h
@@ -2,6 +2,88 @@
 #include "Module.h"
 #include "Globals.h"
 
+
+// Class: Ball object (a simple stoopid physics object)
+class PhysBall
+{
+public:
+	// Position
+	// You could also use an array/vector
+	float x;
+	float y;
+
+	// Velocity
+	float vx;
+	float vy;
+
+	// Acceleration
+	float ax;
+	float ay;
+
+	// Force (total) applied to the ball
+	float fx;
+	float fy;
+
+	// Mass
+	float mass;
+
+	// Aerodynamics stuff
+	float surface; // Effective wet surface
+	float cl; // Aerodynamic Lift coefficient
+	float cd; // Aerodynamic Drag coefficient
+	float b; // Hydrodynamic Drag coefficient
+
+	// Coefficients of friction & restitution (for bounces)
+	float coef_friction;
+	float coef_restitution;
+
+	// Shape
+	float radius;
+
+	// Has physics enabled?
+	bool physics_enabled = true;
+};
+
+class PhysCube
+{
+public:
+	// Position
+	// You could also use an array/vector
+	float x;
+	float y;
+
+	// Velocity
+	float vx;
+	float vy;
+
+	// Acceleration
+	float ax;
+	float ay;
+
+	// Force (total) applied to the ball
+	float fx;
+	float fy;
+
+	// Mass
+	float mass;
+
+	// Aerodynamics stuff
+	float surface; // Effective wet surface
+	float cl; // Aerodynamic Lift coefficient
+	float cd; // Aerodynamic Drag coefficient
+	float b; // Hydrodynamic Drag coefficient
+
+	// Coefficients of friction & restitution (for bounces)
+	float coef_friction;
+	float coef_restitution;
+
+	// Shape
+	float width;
+	float height;
+
+	// Has physics enabled?
+	bool physics_enabled = true;
+};
 class ModulePhysics : public Module
 {
 public:
@@ -12,6 +94,9 @@ public:
 	update_status PreUpdate();
 	update_status PostUpdate();
 	bool CleanUp();
+
+	PhysBall balls;
+	PhysCube cubes;
 
 private:
 
